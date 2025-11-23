2f611622d8f3d20ac77956424c393240d34901a0
Brian Wong <shinephone2010@gmail.com>
2025-11-24 00:10:05 +1300

    Generated the Base API Controller

---
+++ FlightInformationAPI/Controllers/FlightInformationController.cs
@@ -0,0 +1,45 @@
+﻿using FlightInformation.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace FlightInformationAPI.Controllers
+{
+    public class FlightInformationController : FlightInformationControllerControllerBase
+    {
+        [HttpPost]
+        public override Task<ActionResult<Flight>> CreateFlight(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public override Task<IActionResult> DeleteFlight([BindRequired] string id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public override Task<ActionResult<Flight>> GetFlight([BindRequired] string id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public override Task<ActionResult<ICollection<Flight>>> GetFlights(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public override Task<ActionResult<ICollection<Flight>>> SearchFlight([FromQuery] string airline = null, [FromQuery] string departureAirport = null, [FromQuery] string arrivalAirport = null, [FromQuery] string fromDate = null, [FromQuery] string toDate = null, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public override Task<ActionResult<Flight>> UpdateFlight([BindRequired] string id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- OpenAPI/Src/Stub/FlightInformation.API/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FlightInformation.API
-{
-    public class Class1
-    {
-
-    }
-}
